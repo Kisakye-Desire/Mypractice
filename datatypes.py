e27ba352587e9b7d8ff77cc8514846a08f4b6d20
Kisakye Dhisayar <92805452+Kisakye-Desire@users.noreply.github.com>
2022-10-21 00:24:40 -0700

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,54 @@
+
+#Printing on the terminal
+#print('Hello World!!')
+
+#Declaring
+#name="Eliza"
+#rint("Hello", name)
+
+#Asking for user input
+#name=input("What is your name?")
+#print("My name is",name)
+
+#age=input("What is your age?")
+#print("My age is",age)
+
+#Asking for user input ,casting and displaying the result.
+#name1=input("Hey!!What is your name?")
+#item=input("Your welcome.What items would you like to shop?")
+#cost=float(input("The cost of the item is?"))
+#quantity=float(input("I am taking "))
+#total_cost=cost*quantity
+
+#print("My name is",name)
+#print("I am taking",item)
+#print("The item costs",cost)
+#print("I am taking",quantity)
+#print("Your bill is ",total_cost)
+
+#Multiple strings
+#print("Hey am Desire","I Love god","I am a Ugandan")
+
+#Conditions
+number1=int(input("Enter your first Number:"))
+number2=int(input("Enter your second Number:"))
+
+if number1>number2:
+    print("Number 1 is greater than Number2")
+else:
+     print("Number 1 is less than Number 2")
+
+
+#Using elif
+
+num1=int(input("First Number:"))
+num2=int(input("Second Number:"))
+
+if num1 == num2:
+    print("Number 1 is equal to Number 2!")
+
+elif num1>num2:
+    print("Number 1 is greater than Number2!")
+
+else: 
+    print("Number 1 is less than Number 2")
