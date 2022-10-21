df8982f38e7ce97feb719361080e9da92d659dff
Kisakye Dhisayar <92805452+Kisakye-Desire@users.noreply.github.com>
2022-10-21 00:25:32 -0700

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,24 @@
+
+#Print Functions
+
+#print("Hii!!")
+
+#Writing functions in python we use def
+
+def greet_user( name):
+
+   print("Hello", name)
+greet_user("Desire")
+
+#Second Function
+
+def great_user( greet_statement,name):
+
+    print(greet_statement, name)
+great_user("Howdy","Desire")
+
+#Function to add Numbers
+def add_numbers( num1,num2):
+    return num1+num2
+
+print(add_numbers(3,6))
