a01b05674c01fcb3fb60a4ae8cd68befc441fb94
Kisakye Dhisayar <92805452+Kisakye-Desire@users.noreply.github.com>
2022-10-21 00:26:48 -0700

    Add files via upload

---
+++ Week4.py
@@ -0,0 +1,14 @@
+print('Hello,World!',end="***")
+print('Hello World!')
+
+#printing to the next line
+print('Hello,World!',end="\n")
+print('Hello World!')
+
+name="Lizzy"
+#printing a string with something in the middle.
+print('Hello', name ,sep="+++")
+
+#Taking in information
+name1=input("What is your name?")
+print('Hello', name1)
